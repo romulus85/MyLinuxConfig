4a764e0e36134f73a151c5c2df5003bcaf314597
Francisco Javier Castaño Gómez <franciscojavier.castanogomez@telefonica.com>
2026-03-12 11:54:35 +0100

    Sway 11 changes

--- sway/autostart-apps.sh
+++ sway/autostart-apps.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Función que comprueba si ya hay una ventana con un app_id o instance dados
 already_running() {
   swaymsg -t get_tree | grep -q "\"$1\": \"$2\""
 }
 
 sleep 10
+
 # Workspace 1
 swaymsg "workspace 1"
-if ! already_running "instance" "jetbrains-idea"; then
+if ! already_running "class" "jetbrains-idea"; then
   swaymsg exec '/home/cx02913/.local/share/JetBrains/Toolbox/scripts/idea-ultimate'
   sleep 10
 fi
-
 if ! already_running "app_id" "kitty"; then
   swaymsg exec 'kitty'
   sleep 0.5
@@ -21,19 +20,19 @@ if ! already_running "app_id" "kitty"; then
 fi
 
 # Workspace 2
-if ! already_running "instance" "microsoft-edge"; then
+swaymsg "workspace 2"
+if ! already_running "class" "microsoft-edge"; then
   swaymsg exec 'microsoft-edge'
 fi
 
 # Workspace 3
-if ! already_running "instance" "spotify"; then
+swaymsg "workspace 3"
+if ! already_running "class" "Spotify"; then
   swaymsg exec 'spotify'
 fi
-
-if ! already_running "app_id" "telegram-desktop_telegram-desktop"; then
+if ! already_running "app_id" "org.telegram.desktop"; then
   swaymsg exec 'telegram-desktop'
 fi
-
-if ! already_running "instance" "crx__hnpfjngllnobngcgfapefoaidbinmjnm"; then
+if ! already_running "class" "crx__hnpfjngllnobngcgfapefoaidbinmjnm"; then
   swaymsg exec 'microsoft-edge --app-id=hnpfjngllnobngcgfapefoaidbinmjnm'
 fi
